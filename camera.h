7acdd370387a9ee7ba64c4eb96861920e01a79fc
Michal Klos <michal.m.klos@gmail.com>
2018-04-16 23:58:21 +0200

    ch10 positionable camera

--- camera.h
+++ camera.h
@@ -4,11 +4,22 @@
 
 class camera {
 	public: 
-		camera() {
-			lowerLeftCorner = vec3(-2.0f, -1.0f, -1.0f);
-			horizontal = vec3(4.0f, 0.0f, 0.0f);
-			vertical = vec3(0.0f, 2.0f, 0.0f);
-			origin = vec3(0.0f, 0.0f, 0.0f);
+		camera(vec3 lookFrom, vec3 lookAt, vec3 up, float vfov, float aspect) { // vfov: top-bottom, degrees
+			
+			float theta = vfov * M_PI / 180.0f;
+			float halfHeight = tan(theta / 2.0f);
+			float halfWidth = aspect * halfHeight;
+
+			vec3 u, v, w;
+			origin = lookFrom;
+			w = normalize(lookFrom - lookAt);
+			u = normalize(cross(up, w));
+			v = cross(w, u);
+
+			lowerLeftCorner = vec3(-halfWidth, -halfHeight, -1.0f);
+			lowerLeftCorner = origin - halfWidth * u - halfHeight * v - w;
+			horizontal = 2.0f * halfWidth * u;
+			vertical =  2.0f * halfHeight * v;
 		}
 
 		ray getRay(float u, float v) { return ray(origin, lowerLeftCorner + u*horizontal + v*vertical - origin); }
