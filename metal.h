e22178fe923e0d65dec311afec65c22cdaaaaa13
Michal Klos <michal.m.klos@gmail.com>
2018-04-16 22:08:58 +0200

    ch8 added metal fuzz

--- metal.h
+++ metal.h
@@ -5,14 +5,15 @@
 
 class metal : public material {
 	public: 
-		metal(const vec3& a) : albedo(a) {}
+		metal(const vec3& a, float f) : albedo(a), fuzz(f) {}
 		
 		virtual bool scatter(const ray & in, const hitRecord & hit, vec3 & attentuation, ray & scattered) const override {
 			vec3 reflected = reflect(normalize(in.dir()), hit.normal);
-			scattered = ray(hit.point, reflected);
+			scattered = ray(hit.point, reflected + fuzz * randomInUnitSphere());
 			attentuation = albedo;
 			return (dot(scattered.dir(), hit.normal) > 0.0f);
 		};
 
 		vec3 albedo;
+		float fuzz;
 };
